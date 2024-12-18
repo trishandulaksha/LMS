24152011839c200866b3a8abb9507b4913d489f2
pasindu <pasindunawagamuwa4@gmail.com>
2024-12-18 13:41:35 +0530

    responsivesomepages

--- Client/src/screen/Schedule/Schedule.js
+++ Client/src/screen/Schedule/Schedule.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import './Schedule.css'
 // Required styles for FullCalendar
 //import "@fullcalendar/core/styles.css";
 //import "@fullcalendar/daygrid/styles.css";
