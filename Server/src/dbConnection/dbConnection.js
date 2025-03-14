6b56cf62e7daf2c94fcb3fb2ab1c6bc2cb900804
trishandulaksha <trishandulaksha@gmail.com>
2025-03-14 21:39:02 +0530

    before the merge

--- Server/src/dbConnection/dbConnection.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const databaseSetup = async () => {
-  await mongoose.connect(process.env.DB_URI);
-};
--- Server/src/router/studentMarksRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getMarksByStudentId } from "../user-ep/studentMarks-ep.js";
-
-const studentMarksRouter = Router();
-
-studentMarksRouter.post("/:studentID/getStudentMarks", getMarksByStudentId);
-
-export default studentMarksRouter;
